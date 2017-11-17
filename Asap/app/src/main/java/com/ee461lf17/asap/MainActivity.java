22b0e6db35c20c231b654bec8ea4ad6b36213430
emily-tran-36 <emily.tran36@utexas.edu>
2017-11-16 23:08:03 -0600

    content for listview of budget and accounts

--- Asap/app/src/main/java/com/ee461lf17/asap/MainActivity.java
+++ Asap/app/src/main/java/com/ee461lf17/asap/MainActivity.java
@@ -1,26 +1,29 @@
 package com.ee461lf17.asap;
 
-import android.support.design.widget.TabLayout;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.NavigationView;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
-import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,6 +44,9 @@ public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawer;
     private NavigationView nvDrawer;
+
+//    private ListView mainListView ;
+//    private ArrayAdapter<String> listAdapter ;
 
 
     @Override
@@ -80,6 +86,8 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
 
     }
 
@@ -169,8 +177,35 @@ public class MainActivity extends AppCompatActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            //TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+            //textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+
+            int viewNumber = getArguments().getInt(ARG_SECTION_NUMBER);
+            if(viewNumber == 1){
+                //Budget List
+                String[] budgets = new String[] { "Monthy", "Fun", "House"};
+                ArrayList<String> budgetList = new ArrayList<String>();
+                budgetList.addAll( Arrays.asList(budgets) );
+                // Create ArrayAdapter using the budget list.
+                ListAdapter budgetAdapter = new ArrayAdapter<String>(getActivity(), R.layout.simplerow, budgetList);
+                ListView mainListView = (ListView) rootView.findViewById( R.id.mainListView );
+                mainListView.setAdapter( budgetAdapter );
+            }
+            else if(viewNumber == 2){
+                String[] accounts = new String[] { "Checking", "Personal Checking"};
+                ArrayList<String> accountsList = new ArrayList<String>();
+                accountsList.addAll( Arrays.asList(accounts) );
+                // Create ArrayAdapter using the budget list.
+                ListAdapter listAdapter = new ArrayAdapter<String>(getActivity(), R.layout.simplerow, accountsList);
+                ListView mainListView = (ListView) rootView.findViewById( R.id.mainListView );
+                mainListView.setAdapter( listAdapter );
+            }
+            else{
+                
+            }
+
+
+
             return rootView;
         }
     }
